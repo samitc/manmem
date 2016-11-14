f371ce4f04316c7d7c426531b10d962646190033
Amit Cirt <siamitcs0@gmail.com>
2016-11-14 21:59:02 +0200

    Change project layout

--- Src/Pools/ShortPoolMemory.h
+++ Src/Pools/ShortPoolMemory.h
@@ -1,6 +1,6 @@
 #ifndef _SHORTPOOLMEMORY_H_
 #define _SHORTPOOLMEMORY_H_
-#include "GeneralTypes.h"
+#include "../../GeneralTypes.h"
 #if !defined(SHORT_POOL_MEM_SIZE)
     #define SHORT_POOL_MEM_SIZE 1024*1024
 #endif
